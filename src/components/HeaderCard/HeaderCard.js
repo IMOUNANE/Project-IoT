85cb3ec978c5f886dfa42a9c7dea002b7c088f51
Mauduit-O <oum-el-khaire.mauduit@hetic.net>
2021-07-21 19:21:41 +0200

    add chart WaterQuality

--- src/components/HeaderCard/HeaderCard.js
+++ src/components/HeaderCard/HeaderCard.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./HeaderCard.css";
 
 export default function HeaderCard(props){
-    const {title,subtitle}=props;
+    const {title,infoQuality,subtitle}=props;
 
   
 
  return(
      <div className="header-container d-flex justify-content-between align-items-center p-3">
-         <h1 className="header-title p-3">{title}</h1>
+         <h1 className="header-title p-3">{title} <span className="infoQuality">{infoQuality}</span></h1>
          <h2 className="subtitle">{subtitle} </h2>
      </div>
  )
